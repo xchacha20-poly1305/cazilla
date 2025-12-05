4025e56ba19ecc48b65f44e959ae2055c42007b4
安容 <HystericalDragons@proton.me>
2025-12-05 09:55:50 +0800

    Fix broken link

--- const.go
+++ const.go
@@ -3,5 +3,5 @@ package cazilla
 const (
 	// SourceCAListURL is the source to get all CA certificates, provided by Mozilla.
 	// See https://wiki.mozilla.org/CA/Included_Certificates
-	SourceCAListURL = "https://ccadb-public.secure.force.com/mozilla/IncludedCACertificateReportPEMCSV"
+	SourceCAListURL = "https://ccadb.my.salesforce-sites.com/mozilla/IncludedCACertificateReportPEMCSV"
 )
